68c043faa5b0b716e3b654b3c93d5caef9a407c3
pushap1 <31646089+pushap1@users.noreply.github.com>
2021-03-31 01:38:14 +0300

    Merge changes from /nodos/site/ with docusaurus init

---
+++ sidebars.js
@@ -0,0 +1,6 @@
+module.exports = {
+  someSidebar: {
+    'Getting Started': ['getting_started', 'motivation'],
+    'Simple examples with docusaurus': ['doc1'], // I don't delete this, because it contain some hints to work with doc.
+  },
+};
--- site/sidebars.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  someSidebar: {
-    'Getting Started': ['getting_started', 'motivation'],
-    'Simple examples with docusaurus': ['doc1', 'mdx'], // I don't delete this, because it contain some hints to work with doc.
-  },
-};
