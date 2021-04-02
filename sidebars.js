48ea151f699ed8ed2e29e5c20c0be0efd1179cb1
Kirill Mokevnin <mokevnin@gmail.com>
2021-04-03 00:39:53 +0400

    Deploy website - based on 6dc8a2f90631b27e6fdb27aa04fdc8d55ca7480c

--- sidebars.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  someSidebar: {
-    'Start Here': ['getting_started', 'motivation'],
-    'Simple examples with docusaurus': ['doc1'], // I don't delete this, because it contain some hints to work with doc.
-  },
-};
